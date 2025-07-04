66ee17f16e4c881fb65a39f93ff4f09924994845
Takahiro, Fujino <37112060+northbnk@users.noreply.github.com>
2025-07-04 23:48:59 +0900

    Compute elevation gain/loss per km

--- test_app.py
+++
@@ -1,8 +0,0 @@
-import gpxutils
-
-
-def test_parse():
-    with open('testdata/sample.gpx') as f:
-        stats = gpxutils.parse_gpx(f)
-    assert stats['points'] == 2
-    assert round(stats['distance_m'], 1) > 0
